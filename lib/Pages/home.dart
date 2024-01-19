ec3fd643decdcd8ea51c6cb5032e794db10d13d2
Siman Nath <simannath.sn@gmail.com>
2024-01-19 20:16:35 +0530

    firstCommit

---
+++ lib/Pages/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatelessWidget {
+  const homePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Meal',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          )
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+
+      ),
+    );
+  }
+}
